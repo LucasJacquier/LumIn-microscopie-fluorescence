34ddacfcf9ad80bf0441030a0d255f6b015fc150
LucasJacquier <113975558+LucasJacquier@users.noreply.github.com>
2022-09-29 18:52:05 +0200

    Update normalize.py
    Il y a une fonction qui retire les valeurs aberrantes et une fonction qui normalise les données, sauf que ca renvoie un maximum à 256 et non 255
    Je vais essayer de réparer ca

--- normalize.py
+++ normalize.py
@@ -5,14 +5,34 @@ import numpy as np
 
 
 def remove_outliers (film):
-    new_max = np.percentile(film, 0.99)
+    list_outliers = []
+    for i in range(int(np.size(film)/(2*10**6))):
+        new_max = np.amax(film)
+        print(new_max)
+        indice_new_max = np.where(film == new_max)
+        film[indice_new_max] = 0
+        list_outliers.append(indice_new_max)
+    print(len(list_outliers))
+    print(list_outliers)
+    definitive_max = np.amax(film)
+    for l in range(int(np.size(film)/(2*10**6))):
+        film[list_outliers[l]] = definitive_max
+    return None
+
+def normalize (film):
+    old_extremes = [np.amin(film), np.amax(film)]
+    new_extremes = [0,255]
+    classe = (old_extremes[1]-old_extremes[0])/256
     dimension = np.shape(film)
     for k in range(dimension[0]):
         for l in range(dimension[1]):
             for m in range(dimension[2]):
-                if film[k,l,m] > new_max:
-                    film[k,l,m] = new_max
-    return np.amax(film)
+                film[k,l,m] = (film[k,l,m]-old_extremes[0]+1)//classe
+    return None
+#Le maximum est de 256 et non 255, il faut régler ce problème :'(
+
+
+
 
 
 
