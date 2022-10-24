1db4390f6cc5bc385033e1cc182a63647d8cd086
LucasJacquier <113975558+LucasJacquier@users.noreply.github.com>
2022-10-24 22:16:33 +0200

    la fonction permettant de détecter les particules et créer les images
    Les fonctions sont documentées. Il y a une fonction simple pour sauvegarder les images repérées, mais que d'une unique couleur (la fonction utilisée pour les appels automatiques).
    Et une fonction plus complète permettant d'avoir les deux couleurs, ainsi que les graphes du nombre de points. C'est d'ailleurs l'affichage de ces graphes qui cause problème. La fonction complète manque encore d'annotations.
    La dernière cellule est le code utilisée pour le lancement automatique de la fonction de repérage sur une gamme de valeurs de paramètres

--- test tp.batch.py
+++ test tp.batch.py
@@ -8,25 +8,42 @@ import re
 from PIL import Image
 from load_data import load_data
 
-##Fonction simplifié
+##Path of the films
+
+#path = 'write the path of the film'
+#path = 'C:/Users/lucas/Documents/sciences/Mes Recherches/2022_09 ARIA 1er Stage/Films/EMCV1_0524_1RRL.tif'
+#path = 'C:/Users/lucas/Documents/Science_ENS/matériel stage ARIA/A6_0831_8RRL.tif'
+
+##Simplified function
 
 
-def image_peak_save_show (path, color_sequence, batch_parameters):
-    '''batch_parameters = [diameter, minmass, percentile]'''
-    film_rouge = load_data(path, color_sequence) ['R'] #chargement du film
+def image_peak_save_show (path : Union[Path, str], color_sequence : List[str], selection : Union[str, int], batch_parameters : list, saving_path : Union[Path, str]):
+    '''Create an image with the detected particles marked with a red dot and save it in a predefined folder. The detection parameters can be modified selecting the batch parameters.
+    Args :
 
-    array_spot_R = tp.batch(film_rouge, diameter = batch_parameters[0], minmass = batch_parameters[1], percentile = batch_parameters[2]) #extraction des points lumineux
+    path (Union[Path, str]): path to the film (e.g. "c:\\users\\john\\documents\\project\\film.tif")
+    color_sequence (List[str]): colors of the frames, in the correct order.
+    selection (List[str, int] = [selected_color, selected_frame) : the color and the frame selected from the film
+    batch_parameters (List[str] = [diameter, minmass, percentile]): the parameters of the tracky.batch function to find the particles
+    saving_path : path to the file where to save the images
+
+    Returns :
+
+        None
+
+    '''
+    film_one_color = load_data(path, color_sequence) [selection[0]] #Film loading
+
+    array_spot_R = tp.batch(film_one_color, diameter = batch_parameters[0], minmass = batch_parameters[1], percentile = batch_parameters[2]) #Peak extraction
 
     ordinate_image0_spot, abscissa_image0_spot = (
     array_spot_R[array_spot_R['frame'] == 0]['y'],
     array_spot_R[array_spot_R['frame'] == 0]['x'])
-
     length = len(ordinate_image0_spot)
+    int_coordinate = [(int(coordinate[i][0]), int(coordinate[i][1])) for i in range(length)] #Establishment of integers peak coordinates
 
-    coordinate = [(ordinate_image0_spot[i], abscissa_image0_spot[i]) for i in range(0,length)]
+    modified_image = Image.new('RGB',(film_shape[1],film_shape[2])) #Creation of a new image
 
-    modified_image = Image.new('RGB',(film_shape[1],film_shape[2]))
-    int_coordinate = [(int(coordinate[i][0]), int(coordinate[i][1])) for i in range(length)]
     for x in range(film_shape[1]):
         for y in range(film_shape[2]):
             color = int(film_rouge[0,x,y])
@@ -35,28 +52,40 @@ def image_peak_save_show (path, color_sequence, batch_parameters):
                 modified_image.putpixel((x,y), (255, 0, 0))
             else:
                 modified_image.putpixel((x,y), (color, color, color))
-            origin_image.putpixel((x,y), (color, color, color))
+            origin_image.putpixel((x,y), (color, color, color)) #Painting of the image with the red dots
 
 
-    name_modif = str(path[re.search('Films/', path).span()[1] : -4] + '_' + str(batch_parameters) + '_R0_detectedpeaks')
+    name_modif = str(path[re.search('Films/', path).span()[1] : -4] + '_' + str(batch_parameters) + '_R0_detectedpeaks') #Name of the new file
 
-    modified_image.save(r'C:\Users\lucas\Documents\sciences\Mes Recherches\2022_09 ARIA 1er Stage\Images\img_auto(2022_10_21)\ ' + name_modif + '.png')
+    modified_image.save(r'C:\Users\lucas\Documents\sciences\Mes Recherches\2022_09 ARIA 1er Stage\Images\img_auto(2022_10_21)\ ' + name_modif + '.png') #Saving of the new image
 
 
 
-##Fonction complète
+##Complete function
 
-def image_peak_save_show_complete (path, color_sequence, batch_parameters):
-    '''batch_parameters = [diameter, minmass, percentile]'''
+def image_peak_save_show_complete (path : Union[Path, str], color_sequence : List[str], frame_selection : int, batch_parameters : list, saving_path : Union[Path, str]):
+    '''Create an image with the detected particles marked with a red dot and save it in a predefined folder. The detection parameters can be modified selecting the batch parameters.
+    Args :
+
+    path (Union[Path, str]): path to the film (e.g. "c:\\users\\john\\documents\\project\\film.tif")
+    color_sequence (List[str]): colors of the frames, in the correct order.
+    frame_selection (int) : the frame selected from the film
+    batch_parameters (List[str]) = [diameter, minmass, percentile]): the parameters of the tracky.batch function to find the particles
+    saving_path : path to the file where to save the images
+
+    Returns :
+
+        None
+
+    '''
+
     film_rouge, film_vert = (
         load_data(path, color_sequence) ['R'],
-        load_data(path, color_sequence) ['G'])
-    #chargement du film
+        load_data(path, color_sequence) ['G']) #chargement du film
 
     array_spot_R, array_spot_G = (
         tp.batch(film_rouge, diameter = batch_parameters[0], minmass = batch_parameters[1], percentile = batch_parameters[2]),
-        tp.batch(film_vert, diameter = batch_parameters[0], minmass = batch_parameters[1], percentile = batch_parameters[2]))
-    #extraction des points lumineux
+        tp.batch(film_vert, diameter = batch_parameters[0], minmass = batch_parameters[1], percentile = batch_parameters[2])) #extraction des points lumineux
 
     # film_shape = np.shape(film_rouge)
     # graph_choice = input('Voulez-vous voir les graphes de l evolution du nombre de spots (y pour oui et n pour non)?')
@@ -84,7 +113,7 @@ def image_peak_save_show_complete (path, color_sequence, batch_parameters):
     coordinate = [(ordinate_image0_spot[i], abscissa_image0_spot[i]) for i in range(0,length)]
 
     modified_image = Image.new('RGB',(film_shape[1],film_shape[2]))
-    #origin_image = Image.new('RGB',(film_shape[1],film_shape[2]))
+    origin_image = Image.new('RGB',(film_shape[1],film_shape[2]))
     int_coordinate = [(int(coordinate[i][0]), int(coordinate[i][1])) for i in range(length)]
     for x in range(film_shape[1]):
         for y in range(film_shape[2]):
@@ -96,46 +125,29 @@ def image_peak_save_show_complete (path, color_sequence, batch_parameters):
                 modified_image.putpixel((x,y), (color, color, color))
             origin_image.putpixel((x,y), (color, color, color))
 
-    # modified_image.show()
-    # origin_image.show()
+    modified_image.show()
+    origin_image.show()
     name_modif = str(path[re.search('Films/', path).span()[1] : -4] + '_' + str(batch_parameters) + '_R0_detectedpeaks')
-    #name_origin = str(path[re.search('Films/', path).span()[1] : -4] + '_' + str(batch_parameters) + '_R0_initialimage')
+    name_origin = str(path[re.search('Films/', path).span()[1] : -4] + '_' + str(batch_parameters) + '_R0_initialimage')
     modified_image.save(r'C:\Users\lucas\Documents\sciences\Mes Recherches\2022_09 ARIA 1er Stage\Images\img_auto\ ' + name_modif + '.png')
-    #origin_image.save(r'C:\Users\lucas\Documents\sciences\Mes Recherches\2022_09 ARIA 1er Stage\Images\img_auto\ ' + name_origin + '.png')
-
-##Test tp.batch
+    origin_image.save(r'C:\Users\lucas\Documents\sciences\Mes Recherches\2022_09 ARIA 1er Stage\Images\img_auto\ ' + name_origin + '.png')
 
 
-#path = 'write the path of the film'
-#path = 'C:/Users/lucas/Documents/sciences/Mes Recherches/2022_09 ARIA 1er Stage/Films/EMCV1_0524_1RRL.tif'
-#path = 'C:/Users/lucas/Documents/Science_ENS/matériel stage ARIA/A6_0831_8RRL.tif'
+##Automatic launch of the simplified function
+list_diameter = [5,7,9]
+list_minmass = [0.01*i for i in range(11,17)]
+list_percentile = [75+1*i for i in range(0,15)] #Range values realization
+
+# path = (e.g. "c:\\users\\john\\documents\\project\\film.tif") #Assign the path value
+
+for diam in range(len(list_diameter)):
+    for mm in range(len(list_minmass)):
+        for pe in range(len(list_percentile)):
+            bp = [list_diameter[diam], list_minmass[mm], list_percentile[pe]]
+            image_peak_save_show (path, 'RG', bp) # loops of calls
 
 
-## Exemple de detection de pics sans debruitage
-from PIL import Image
 
-ordinate_image0_spot, abscissa_image0_spot = (
-array_spot_R[array_spot_R['frame'] == 0]['y'],
-array_spot_R[array_spot_R['frame'] == 0]['x'])
-
-length = len(ordinate_image0_spot)
-
-coordinate = [(ordinate_image0_spot[i], abscissa_image0_spot[i]) for i in range(0,length)]
-
-modified_image = Image.new('RGB',(film_shape[1],film_shape[2]))
-origin_image = Image.new('RGB',(film_shape[1],film_shape[2]))
-int_coordinate = [(int(coordinate[i][0]), int(coordinate[i][1])) for i in range(length)]
-for x in range(film_shape[1]):
-    for y in range(film_shape[2]):
-        color = int(film_rouge[0,x,y])
-        if (x,y) in int_coordinate:
-            int_coordinate.remove((x,y))
-            modified_image.putpixel((x,y), (255, 0, 0))
-        else:
-            modified_image.putpixel((x,y), (color, color, color))
-        origin_image.putpixel((x,y), (color, color, color))
-# modified_image.show()
-# origin_image.show()
 
 
 
