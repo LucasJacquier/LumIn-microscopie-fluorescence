1963dcb4c3e353d5017ec9277d69a1bcd0ccdf82
Charles T <charles@doffy.net>
2022-11-26 12:16:47 +0100

    formatting

--- background.py
+++ background.py
@@ -4,12 +4,10 @@ from typing import Optional
 
 
 def remove_background(
-    image: npt.NDArray,
-    kernel_size: Optional[int] = 101,
-    sigma: Optional[float] = None
+    image: npt.NDArray, kernel_size: Optional[int] = 101, sigma: Optional[float] = None
 ) -> npt.NDArray:
     if sigma is None:
-        sigma = 0.3*((kernel_size-1)*0.5 - 1) + 0.8
+        sigma = 0.3 * ((kernel_size - 1) * 0.5 - 1) + 0.8
     background = cv.GaussianBlur(
         src=image, ksize=(kernel_size, kernel_size), sigmaX=sigma
     )
